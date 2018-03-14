053b9014bc4792279a700eb8854172ef1e10b018
Nucleartom <jacob.maarse@gmail.com>
2018-03-14 16:54:37 +0000

    Completed custom function to map given function and array

---
+++ customMap.js
@@ -0,0 +1,30 @@
+var words = ["ground", "control", "to", "major", "tom"];
+
+map(words, function(word) {
+  return word.length;
+});
+
+map(words, function(word) {
+  return word.toUpperCase();
+});
+
+map(words, function(word) {
+  return word.split('').reverse().join('');
+});
+
+
+
+function map(array, givenFunc){
+var output = [];
+  for (i=0; i <array.length; i++){
+    output[i] = givenFunc(array[i]);
+  }
+  console.log(output);
+}
+
+
+// [6, 7, 2, 5, 3]
+
+// [ "GROUND", "CONTROL", "TO", "MAJOR", "TOM" ]
+
+// [ 'dnuorg', 'lortnoc', 'ot', 'rojam', 'mot' ]
